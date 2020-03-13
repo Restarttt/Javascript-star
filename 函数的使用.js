fd8720eb605fae690a37bdee34433711f6f28550
Your Name <you@example.com>
2020-03-13 22:29:47 +0800

    3.13循环函数的使用

---
+++ 函数的使用.js
@@ -0,0 +1,59 @@
+// 实现一个函数，获取1-200的既能被3整除也能被5整数的数的最大值
+
+var max = 0;
+
+function getSum() {
+    for (var a = 1; a <= 200; a++) {
+        if (a % 3 == 0 && a % 8 == 0) {
+            max = a;
+
+
+        }
+    }
+    return (max);
+
+}
+console.log(getSum())
+
+// 实现一个函数，获取1-200的总和
+var sum = 0;
+
+function getSum() {
+    for (var a = 1; a <= 200; a++) {
+        sum += a;
+    }
+    return (sum);
+}
+
+console.log(getSum());
+
+
+// 实现一个函数，获取1-200中奇数的总和
+var sum = 0;
+
+function getSum() {
+    for (var a = 0; a <= 200; a++) {
+        if (a % 2 != 0) {
+            sum += a;
+        }
+    }
+    return (sum);
+}
+console.log(getSum());
+
+
+// 实现一个函数，判断传入的参数是否是偶数，如果是，则返回true ，如果不是则返回false
+function getSum(a) {
+    if (a % 2 == 0) {
+        a = true;
+
+
+    } else if (a % 2 != 0) {
+        a = false;
+
+    }
+    return (a);
+
+}
+// getSum(41);
+console.log(getSum(923));
