737baee18517ba70d936dc7ceedf8f40716cd01d
Your Name <you@example.com>
2020-03-18 15:43:45 +0800

    3.18简单算法的练习

--- 简单的算法练习.js
+++ 简单的算法练习.js
@@ -40,22 +40,46 @@ function getnum(arr) {
 }
 console.log(getnum([1, 2312, 1, -33, 44, 12, -1]))
 
-// 3、找出数组中第二大的数、第二小的数
+// 3、找出数组中第二大的数
+
 
 function getnum(arr) {
-    var max = arr[0];
-    var two = 0;
+    var max = (arr[0] > arr[1] ? arr[0] : arr[1])
+    var secondmax = (arr[0] > arr[1] ? arr[1] : arr[0])
+
     for (var i = 0; i < arr.length; i++) {
         if (arr[i] > max) {
+            secondmax = max;
             max = arr[i];
+            console.log(max);
+        } else if (arr[i] > secondmax) {
+            secondmax = arr[i];
+            console.log(secondmax, i);
+        }
 
-            console.log(max, i);
+    }
+    return secondmax;
+}
+console.log(getnum([100, 35, 31, 200, 210, 10, 50]))
+
+// 3、找出数组中第二小的数
+
+function getsecondmin(arr) {
+    var min = (arr[0] < arr[1] ? arr[0] : arr[1])
+    var secondmin = (arr[0] < arr[1] ? arr[1] : arr[0])
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            secondmin = min;
+            min = arr[i];
+            console.log(min);
+        } else if (arr[i] < secondmin) {
+            secondmin = arr[i];
+            console.log(secondmin, i);
         }
     }
-    return max;
+    return secondmin;
 }
-console.log(getnum([20, 35, 31, -10, 26, 10, 50]))
-
+console.log(getsecondmin([100, 35, 31, 200, 26, 10, 50]))
 
 // 4、找出数组中最大的偶数
 
@@ -67,11 +91,11 @@ function geteven(arr) {
                 maxeven = arr[i];
                 console.log(maxeven, i);
             }
-
         }
     }
     return maxeven;
 }
+
 console.log(geteven([2311, 1, -33, 44, 12, -1, 2400]))
 
 
@@ -79,13 +103,12 @@ console.log(geteven([2311, 1, -33, 44, 12, -1, 2400]))
 [1, 2, 78, 23, 2, 23] => [1, 2, 78, 23]
 
 function getnum(arr) {
-    var a = 0;
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i] == arr[i]) {
+        if (arr[i] == arr[i + 1]) {
             a = arr.indexof(3);
         }
     }
-    return a;
+    return
 }
 
 console.log(serach([3, -3, -3, 6, 9, -11, 10, -11, -11, 6, 0]))
@@ -98,38 +121,57 @@ function searchIndex(arr, target);
 a = [3, -3, 9, -11, 10, 6, 0];
 a.indexOf(0)
 
-// function search(arr) {
-//     search.indexOf(0);
-//     return search;
-
-
-// }
-// console.log(search([3, -3, 9, -11, 10, 6, 0]))
+function search(arr) {
+    var num = 0;
+    num = arr.indexOf(10);
+    return num;
+}
+console.log(search([3, -3, 9, -11, 10, 6, 0]))
 
 
 // 循环遍历方法
 
-function search(arr) {
-    var a = 0;
+function search(arr, num) {
+    var a = -1;
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i] == 10) {
-            a = arr[i];
+        if (arr[i] == num) {
+            a = i;
             console.log(a, i);
+            break;
         }
     }
     return a;
 }
-console.log(search([3, -3, 9, -11, 10, 6, 0]))
+console.log(search([3, -3, 9, -11, 10, 6, 9], 9))
 
 // 7、找出两个数组中相同的数
 
 
-// 8、从有序数组中，查找指定的元素 [循环方法、二分查找法]
+function getnum(arr1, arr2) {
+    var num = 0;
+    // var arr = (arr1.length > arr2.length ? arr1.length : arr2.length);
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr1[i] == arr2[i]) {
+            num = arr1[i];
+            console.log(i);
 
-// 9、实现数组排序
-[1, 56, 23, 2, 78, 34] => [1, 2, 23, 34, 56, 78]
 
-// 10、实现Fibornacci数列 [递归版本、非递归版本]
-1 1 2 3 5 8 13 21
+        }
 
-// 10、学习快速排序
+
+    }
+    return num;
+
+}
+console.log(getnum(([3, -3, 9, -11, 10, 6, 0], [3, 31, 9, -11, 8, 6, 1]))
+
+
+        // 8、从有序数组中，查找指定的元素 [循环方法、二分查找法]
+
+        // 9、实现数组排序
+        [1, 56, 23, 2, 78, 34] => [1, 2, 23, 34, 56, 78].
+
+        // 10、实现Fibornacci数列 [递归版本、非递归版本]
+        1 1 2 3 5 8 13 21
+
+        // 10、学习快速排序
