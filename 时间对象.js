604df39dbfc4fa0c044e2e9f01f2c311c8a0f09b
Your Name <you@example.com>
2020-03-21 16:03:38 +0800

    3.21对象

---
+++ 时间对象.js
@@ -0,0 +1,30 @@
+// Date日期的对象   new是创建一个对象
+// 当前执行的代码的时间
+
+
+//时间戳
++now
+// 
+
+
+
+let = new Date();
+console.log(let.getMinutes())
+console.log(let.getFullYear())
+console.log(let.getMonth() + 1)
+console.log(let.getDate())
+console.log(let.getDay())
+console.log(let.getHours())
+console.log(let.getSeconds())
+
+VM280: 2 0
+VM280: 3 2020
+VM280: 4 2
+VM280: 5 21
+VM280: 6 6
+VM280: 7 16
+VM280: 8 50
+
+2020.3 .21
+16: 00: 50 s
+Sat Mar 21 2020 16: 03: 00 GMT + 0800(中国标准时间)
